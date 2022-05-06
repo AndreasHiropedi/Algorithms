9f518a5ab51ba7294e5461061983f87d619d3deb
Andreas Hiropedi <s2015345@ed.ac.uk>
2022-05-06 16:42:16 +0300

    Update floydwarshall.py

--- floydwarshall.py
+++ floydwarshall.py
@@ -1,6 +1,6 @@
 def FloydWarshall(n):
     inf = 99999
-    # can change these values accordignly
+    # can change these accordingly
     dists = [ [0, 5, inf, 2, inf],
               [inf, 0, 4, -2, 5],
               [inf, inf, 0, inf, inf],
@@ -11,18 +11,12 @@ def FloydWarshall(n):
             for j in range(n):
                 if i != j and (dists[i][k] + dists[k][j] < dists[i][j]):
                     dists[i][j] = dists[i][k] + dists[k][j]
-        print("For k = ", k)
-        print("")
-        print_sol(n, dists)
-        print(" ")
-
-
-def print_sol(n, D):
+    # printing out the final matrix
     for i in range(n):
         for j in range(n):
-            # ensures infinity is printed instead of an actual numerical value
-            if D[i][j] > 999:
+            # ensures infinity is printed
+            if dists[i][j] > 999:
                 print("inf", end = " ")
             else:
-                print(D[i][j], end = " ")
+                print(dists[i][j], end = " ")
         print(" ")
